37d978ed0efec23285a8d26bfc9d939866cca8a0
thiagola92 <thiagola92@gmail.com>
2026-01-10 17:48:21 -0300

    Create BarButton that can work as button or be dragged

--- bar_button/bar_button.gd
+++ bar_button/bar_button.gd
@@ -4,15 +4,15 @@ extends Button
 ## Signal when the button is pressed without leaving the button.
 signal pressed_without_leaving
 
-## Signal when the mouse leave the button without releasing it.
-signal mouse_exited_without_releasing
-
 var _left_once: bool = false
 
 
 func _ready() -> void:
 	button_down.connect(_on_button_down)
+	gui_input.connect(_on_gui_input)
 	mouse_exited.connect(_on_mouse_exited)
+	
+	mouse_filter = Control.MOUSE_FILTER_PASS
 
 
 func _pressed() -> void:
@@ -24,8 +24,12 @@ func _on_button_down() -> void:
 	_left_once = false
 
 
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if not _left_once:
+			get_viewport().set_input_as_handled()
+
+
 func _on_mouse_exited() -> void:
 	if button_pressed:
 		_left_once = true
-		
-		mouse_exited_without_releasing.emit()
